b25010327044d4b3eb8ee9c4fc3f077bd8bb77c4
nl.ruslan <nl.ruslan@yandex.ru>
2019-03-17 22:04:03 +0600

    Исправил пользователя

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM jenkins/jenkins:lts
 
-COPY /init.d/basic-security.groovy  /usr/share/jenkins/ref/init.groovy.d/basic-security.groovy
 COPY /init.d/init.plugins.groovy /usr/share/jenkins/ref/init.groovy.d/init.plugins.groovy
+COPY /init.d/basic-security.groovy  /usr/share/jenkins/ref/init.groovy.d/basic-security.groovy
+
 
 
--- init.d/basic-security.groovy
+++ init.d/basic-security.groovy
@@ -6,12 +6,12 @@ def instance = Jenkins.getInstance()
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 def users = hudsonRealm.getAllUsers()
 users_s = users.collect { it.toString() }
-if ('admin' in users_s) {
+if ('administrator' in users_s) {
     println "Admin user already exists"
 } else {
     println "--> creating local admin user"
 
-    hudsonRealm.createAccount('admin', 'secretPassword')
+    hudsonRealm.createAccount('administrator', 'secretPassword')
     instance.setSecurityRealm(hudsonRealm)
 
     def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
